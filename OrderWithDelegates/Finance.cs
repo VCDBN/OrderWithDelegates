772673e51bb26b7ca5e69b28cdb50582a4b98526
eadam <eb.adam@gmail.com>
2023-05-05 15:18:34 +0200

    Adding Finance class with subscription

---
+++ OrderWithDelegates/Finance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderWithDelegates
+{
+    public class Finance
+    {
+        public Finance(OrderProcessor orderProcessor)
+        {
+            orderProcessor.OrderConfirmed += OnOrderConfirmedExpectFunds;
+        }
+
+        private void OnOrderConfirmedExpectFunds(Order order)
+        {
+            Console.WriteLine("Order made for " + order.CustomerName + 
+                " - Expect payment");
+        }
+    }
+}
--- OrderWithDelegates/Program.cs
+++ OrderWithDelegates/Program.cs
@@ -9,6 +9,8 @@ internal class Program
         Customer customer = new Customer(orderProcessor);
         customer.Name = "Chuck Norris";
 
+        Finance fin = new Finance(orderProcessor);
+
         Order order = new Order { OrderNumber = 12345, CustomerName = customer.Name, ShippingAddress = "12 Radar Drive" };
         orderProcessor.ConfirmOrder(order);
         orderProcessor.ShipOrder(order);
